d849c91dd74b06187fc9be3790d098051783273a
harjas-romana <harjas42@icloud.com>
2026-03-13 00:38:15 +0530

    Fixed config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://e-commerce-v2-j5dl.onrender.com',
         changeOrigin: true,
       },
     },
